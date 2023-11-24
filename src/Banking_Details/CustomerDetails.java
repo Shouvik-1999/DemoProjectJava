fdcedec4ba5e57047154219bfb87499caede9100
Shouvik <krshouvik@gmail.com>
2023-11-24 19:16:49 +0530

    Added CustomerDetails

---
+++ src/Banking_Details/CustomerDetails.java
@@ -0,0 +1,27 @@
+package Banking_Details;
+
+public class CustomerDetails {
+	private String name;
+	private String ID;
+	private float amount;
+	private float balance;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	
+}
